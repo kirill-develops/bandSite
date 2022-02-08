01db1d92e3ad4ccc1eb4be35379da33a62da98e1
Kirill Tchentsov <kirill.develops@gmail.com>
2022-02-07 21:20:52 -0500

    added new file structure paths for scripts and pages. Began the html structure for shows

---
+++ scripts/shows-database.js
@@ -0,0 +1,31 @@
+futureDates = [{
+    date: 'Mon Sept 06 2021',
+    venue: "Ronald Lane",
+    city: "San Francisco, CA",
+    remainingTickets: null
+}, {
+    date: 'Tue Sept 21 2021',
+    venue: "Pier 3 East",
+    city: "San Francisco, CA",
+    remainingTickets: null
+}, {
+    date: 'Fri Oct 15 2021',
+    venue: "View Lounge",
+    city: "San Francisco, CA",
+    remainingTickets: null
+}, {
+    date: 'Sat Nov 06 2021',
+    venue: "Hyatt Agency",
+    city: "San Francisco, CA",
+    remainingTickets: null
+}, {
+    date: 'Fri Nov 26 2021',
+    venue: "Moscow Center",
+    city: "San Francisco, CA",
+    remainingTickets: null
+}, {
+    date: 'Wed Dec 15 2021', 
+    venue: "Press Club",
+    city: "San Francisco, CA",
+    remainingTickets: null
+}]
