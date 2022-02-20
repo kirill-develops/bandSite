7f5adb1f4f1ee64e2727847f11d3e59936043326
Kirill Tchentsov <kirill.develops@gmail.com>
2022-02-20 01:28:55 -0500

    incoporated remaining sprint-one feedback aswell as sprint-two feedback

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,119 +1,119 @@
 createShowTabletHeaders = () => {
-    const dateHeader = document.createElement('h4');
-    dateHeader.classList.add('show__header--tablet','show__header--tablet-first');
-    dateHeader.innerText = 'DATE';
+   const dateHeader = document.createElement('h4');
+   dateHeader.classList.add('show__header--tablet', 'show__header--tablet-first');
+   dateHeader.innerText = 'DATE';
 
-    const venueHeader = document.createElement('h4');
-    venueHeader.classList.add('show__header--tablet');
-    venueHeader.innerText = 'VENUE';
+   const venueHeader = document.createElement('h4');
+   venueHeader.classList.add('show__header--tablet');
+   venueHeader.innerText = 'VENUE';
 
-    const locationHeader = document.createElement('h4');
-    locationHeader.classList.add('show__header--tablet');
-    locationHeader.innerText = 'LOCATION';
+   const locationHeader = document.createElement('h4');
+   locationHeader.classList.add('show__header--tablet');
+   locationHeader.innerText = 'LOCATION';
 
-    const spaceHolder = document.createElement('div');
-    spaceHolder.classList.add('show__tablet-display-holder');
-    
-    const tabletBlock = document.createElement('div');
-    tabletBlock.classList.add('show__block--tablet');
-    tabletBlock.append(dateHeader, venueHeader, locationHeader, spaceHolder);
-    return tabletBlock;
+   const spaceHolder = document.createElement('div');
+   spaceHolder.classList.add('show__tablet-display-holder');
+
+   const tabletBlock = document.createElement('div');
+   tabletBlock.classList.add('show__block--tablet');
+   tabletBlock.append(dateHeader, venueHeader, locationHeader, spaceHolder);
+   return tabletBlock;
 }
 createShowStructure = () => {
-    const showContainer = document.createElement('div');
-    showContainer.classList.add('shows-main__container');
+   const showContainer = document.createElement('div');
+   showContainer.classList.add('shows-main__container');
 
-    const showTitle = document.createElement('h2');
-    showTitle.classList.add('shows-main__title');
-    showTitle.innerText = 'Shows';
+   const showTitle = document.createElement('h2');
+   showTitle.classList.add('shows-main__title');
+   showTitle.innerText = 'Shows';
 
-    showContainer.appendChild(showTitle);
-    return [showContainer,showTitle];
+   showContainer.appendChild(showTitle);
+   return [showContainer, showTitle];
 }
 createShowBlock = (show) => {
-    const dateHeader = document.createElement('h4');
-    dateHeader.classList.add('show__header');
-    dateHeader.innerText = 'DATE';
-    
-    const dateInfo = document.createElement('h4');
-    dateInfo.classList.add('show__details', 'show__details--strong');
-    dateInfo.innerText = show.date;
+   const dateHeader = document.createElement('h4');
+   dateHeader.classList.add('show__header');
+   dateHeader.innerText = 'DATE';
 
-    const venueHeader = document.createElement('h4');
-    venueHeader.classList.add('show__header');
-    venueHeader.innerText = 'VENUE';
+   const dateInfo = document.createElement('h4');
+   dateInfo.classList.add('show__details', 'show__details--strong');
+   dateInfo.innerText = show.date;
 
-    const venueInfo = document.createElement('h4');
-    venueInfo.classList.add('show__details')
-    venueInfo.innerText = show.venue;
+   const venueHeader = document.createElement('h4');
+   venueHeader.classList.add('show__header');
+   venueHeader.innerText = 'VENUE';
 
-    const locationHeader = document.createElement('h4');
-    locationHeader.classList.add('show__header');
-    locationHeader.innerText = 'LOCATION';
+   const venueInfo = document.createElement('h4');
+   venueInfo.classList.add('show__details')
+   venueInfo.innerText = show.venue;
 
-    const locationInfo = document.createElement('h4');
-    locationInfo.classList.add('show__details')
-    locationInfo.innerText = show.city;
+   const locationHeader = document.createElement('h4');
+   locationHeader.classList.add('show__header');
+   locationHeader.innerText = 'LOCATION';
 
-    const button = document.createElement('button');
-    button.classList.add('show__button');
-    button.innerText = "BUY TICKETS";
+   const locationInfo = document.createElement('h4');
+   locationInfo.classList.add('show__details')
+   locationInfo.innerText = show.city;
 
-    const container = document.createElement('div');
-    container.classList.add('show__block');
-    container.append(dateHeader, dateInfo, venueHeader, venueInfo, locationHeader, locationInfo, button);
-    return container;
+   const button = document.createElement('button');
+   button.classList.add('show__button');
+   button.innerText = "BUY TICKETS";
+
+   const container = document.createElement('div');
+   container.classList.add('show__block');
+   container.append(dateHeader, dateInfo, venueHeader, venueInfo, locationHeader, locationInfo, button);
+   return container;
 }
 createShowBlockApi = (show) => {
-    const dateHeader = document.createElement('h4');
-    dateHeader.classList.add('show__header');
-    dateHeader.innerText = 'DATE';
+   const dateHeader = document.createElement('h4');
+   dateHeader.classList.add('show__header');
+   dateHeader.innerText = 'DATE';
 
-    const dateInfo = document.createElement('h4');
-    dateInfo.classList.add('show__details', 'show__details--strong');
-    const date = new Date(Number(show.date)).toDateString();
-    console.log(date)
-    const timeDateString = `${date}`
-    dateInfo.innerText = timeDateString;
+   const dateInfo = document.createElement('h4');
+   dateInfo.classList.add('show__details', 'show__details--strong');
+   const date = new Date(Number(show.date)).toDateString();
+   console.log(date)
+   const timeDateString = `${date}`
+   dateInfo.innerText = timeDateString;
 
-    const venueHeader = document.createElement('h4');
-    venueHeader.classList.add('show__header');
-    venueHeader.innerText = 'VENUE';
+   const venueHeader = document.createElement('h4');
+   venueHeader.classList.add('show__header');
+   venueHeader.innerText = 'VENUE';
 
-    const venueInfo = document.createElement('h4');
-    venueInfo.classList.add('show__details')
-    venueInfo.innerText = show.place;
+   const venueInfo = document.createElement('h4');
+   venueInfo.classList.add('show__details')
+   venueInfo.innerText = show.place;
 
-    const locationHeader = document.createElement('h4');
-    locationHeader.classList.add('show__header');
-    locationHeader.innerText = 'LOCATION';
+   const locationHeader = document.createElement('h4');
+   locationHeader.classList.add('show__header');
+   locationHeader.innerText = 'LOCATION';
 
-    const locationInfo = document.createElement('h4');
-    locationInfo.classList.add('show__details')
-    locationInfo.innerText = show.location;
+   const locationInfo = document.createElement('h4');
+   locationInfo.classList.add('show__details')
+   locationInfo.innerText = show.location;
 
-    const button = document.createElement('button');
-    button.classList.add('show__button');
-    button.innerText = "BUY TICKETS";
+   const button = document.createElement('button');
+   button.classList.add('show__button');
+   button.innerText = "BUY TICKETS";
 
-    const container = document.createElement('div');
-    container.classList.add('show__block');
-    container.append(dateHeader, dateInfo, venueHeader, venueInfo, locationHeader, locationInfo, button);
-    return container;
+   const container = document.createElement('div');
+   container.classList.add('show__block');
+   container.append(dateHeader, dateInfo, venueHeader, venueInfo, locationHeader, locationInfo, button);
+   return container;
 }
 // event listener to apply active row class on click
 activeRowListener = () => {
-    const rowsArray = document.querySelectorAll('.show__block');
-    let currentRow = null;
-    rowsArray.forEach(row => {
-        row.addEventListener('click', () => {
-            if (currentRow !== null) {
-                currentRow.classList.remove('show__block--active')
-            }
-            row.classList.add('show__block--active');
-            currentRow = row;
-        })
-    })
+   const rowsArray = document.querySelectorAll('.show__block');
+   let currentRow = null;
+   rowsArray.forEach(row => {
+      row.addEventListener('click', () => {
+         if (currentRow !== null) {
+            currentRow.classList.remove('show__block--active')
+         }
+         row.classList.add('show__block--active');
+         currentRow = row;
+      })
+   })
 }
 
 
@@ -125,22 +125,22 @@ showEl[0].append(tabletHeaders);
 
 const apiShowObj = axios.get(apiShowPage + apiKey);
 apiShowObj.then(result => {
-    console.log(result);
-    result.data.forEach(show => {
-        const newShow = createShowBlockApi(show);
-        showEl[0].append(newShow);
-    })
-    activeRowListener();
+   console.log(result);
+   result.data.forEach(show => {
+      const newShow = createShowBlockApi(show);
+      showEl[0].append(newShow);
+   })
+   activeRowListener();
 })
-    .catch(error => {
-        console.log(error)
-        //use exsisting database
-        futureDates.forEach(show => {
-            const newShow = createShowBlock(show);
-            showEl[0].append(newShow);
-    })
+   .catch(error => {
+      console.log(error)
+      //use exsisting database
+      futureDates.forEach(show => {
+         const newShow = createShowBlock(show);
+         showEl[0].append(newShow);
+      })
 
-});
+   });
 //attach shows to HTML through DOM
 const htmlContainer = document.getElementById('shows');
 htmlContainer.append(showEl[1], showEl[0]);
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,230 +1,230 @@
 // Function to populate HTML into live comment cards from an array database
 // @parma comments is an array of objects that provide an avatar, name, date & comment details for each entry
 displayComment = (comment) => {
-    const avatar = document.createElement('div');
-    avatar.classList.add('live-comment__avatar--blank');
+   const avatar = document.createElement('div');
+   avatar.classList.add('live-comment__avatar--blank');
 
-    const name = document.createElement('h4');
-    name.classList.add('live-comment__name');
+   const name = document.createElement('h4');
+   name.classList.add('live-comment__name');
 
-    const date = document.createElement('h4');
-    date.classList.add('live-comment__date');
+   const date = document.createElement('h4');
+   date.classList.add('live-comment__date');
 
-    const details = document.createElement('p');
-    details.classList.add('live-comment__details');
+   const details = document.createElement('p');
+   details.classList.add('live-comment__details');
 
-    const like = document.createElement('img');
-    like.classList.add('live-comment__icon', 'live-comment__icon--like');
-    like.id = comment.id;
-    like.src = '/assets/icons/svg/icon-like.svg';
-    like.alt = 'like icon';
+   const like = document.createElement('img');
+   like.classList.add('live-comment__icon', 'live-comment__icon--like');
+   like.id = comment.id;
+   like.src = '/assets/icons/svg/icon-like.svg';
+   like.alt = 'like icon';
 
-    const likeCount = document.createElement('h5');
-    likeCount.classList.add('live-comment__like-count');
-    likeCount.id = comment.id;
-    likeCount.innerText = comment.likes;
+   const likeCount = document.createElement('h5');
+   likeCount.classList.add('live-comment__like-count');
+   likeCount.id = comment.id;
+   likeCount.innerText = comment.likes;
 
-    const likeBlock = document.createElement('div');
-    likeBlock.classList.add('live-comment__likes-container');
-    likeBlock.append(likeCount, like);
+   const likeBlock = document.createElement('div');
+   likeBlock.classList.add('live-comment__likes-container');
+   likeBlock.append(likeCount, like);
 
-    const cDelete = document.createElement('img');
-    cDelete.classList.add('live-comment__icon', 'live-comment__icon--delete');
-    cDelete.src = '/assets/icons/svg/icon-delete.svg';
-    cDelete.id = comment.id;
-    cDelete.alt = 'delete icon';
+   const cDelete = document.createElement('img');
+   cDelete.classList.add('live-comment__icon', 'live-comment__icon--delete');
+   cDelete.src = '/assets/icons/svg/icon-delete.svg';
+   cDelete.id = comment.id;
+   cDelete.alt = 'delete icon';
 
-    const left = document.createElement('div');
-    left.classList.add('live-comment__left');
-    left.append(avatar, likeBlock, cDelete);
+   const left = document.createElement('div');
+   left.classList.add('live-comment__left');
+   left.append(avatar, likeBlock, cDelete);
 
-    const right = document.createElement('div');
-    right.classList.add('live-comment__right');
-    right.append(name, date, details);
+   const right = document.createElement('div');
+   right.classList.add('live-comment__right');
+   right.append(name, date, details);
 
-    const liveComment = document.createElement('div');
-    liveComment.classList.add('live-comment');
-    liveComment.append(left, right);
+   const liveComment = document.createElement('div');
+   liveComment.classList.add('live-comment');
+   liveComment.append(left, right);
 
-    const timeDate = new Date(comment.timestamp);
-    const timeDateString = `${timeDate.getMonth() + 1}/${timeDate.getDate()}/${timeDate.getFullYear()}`
-    const relative = timeDifference(timeDate);
+   const timeDate = new Date(comment.timestamp);
+   const timeDateString = `${timeDate.getMonth() + 1}/${timeDate.getDate()}/${timeDate.getFullYear()}`
+   const relative = timeDifference(timeDate);
 
-    name.innerText = comment.name;
-    date.innerText = relative;
-    details.innerText = comment.comment;
+   name.innerText = comment.name;
+   date.innerText = relative;
+   details.innerText = comment.comment;
 
-    document.querySelector('.conversation__output').append(liveComment);
+   document.querySelector('.conversation__output').append(liveComment);
 }
 //prepend new comment to top of comments
 insertComment = (comment) => {
-    const avatar = document.createElement('div');
-    avatar.classList.add('live-comment__avatar--blank');
+   const avatar = document.createElement('div');
+   avatar.classList.add('live-comment__avatar--blank');
 
-    const name = document.createElement('h4');
-    name.classList.add('live-comment__name');
+   const name = document.createElement('h4');
+   name.classList.add('live-comment__name');
 
-    const date = document.createElement('h4');
-    date.classList.add('live-comment__date');
+   const date = document.createElement('h4');
+   date.classList.add('live-comment__date');
 
-    const details = document.createElement('p');
-    details.classList.add('live-comment__details');
+   const details = document.createElement('p');
+   details.classList.add('live-comment__details');
 
-    const like = document.createElement('img');
-    like.classList.add('live-comment__icon', 'live-comment__icon--like');
-    like.id = comment.id;
-    like.src = '/assets/icons/svg/icon-like.svg';
-    like.alt = 'like icon';
+   const like = document.createElement('img');
+   like.classList.add('live-comment__icon', 'live-comment__icon--like');
+   like.id = comment.id;
+   like.src = '/assets/icons/svg/icon-like.svg';
+   like.alt = 'like icon';
 
-    const likeCount = document.createElement('h5');
-    likeCount.classList.add('live-comment__like-count');
-    likeCount.id = comment.id;
-    likeCount.innerText = comment.likes;
+   const likeCount = document.createElement('h5');
+   likeCount.classList.add('live-comment__like-count');
+   likeCount.id = comment.id;
+   likeCount.innerText = comment.likes;
 
-    const likeBlock = document.createElement('div');
-    likeBlock.classList.add('live-comment__likes-container');
-    likeBlock.append(likeCount, like);
+   const likeBlock = document.createElement('div');
+   likeBlock.classList.add('live-comment__likes-container');
+   likeBlock.append(likeCount, like);
 
-    const cDelete = document.createElement('img');
-    cDelete.classList.add('live-comment__icon', 'live-comment__icon--delete');
-    cDelete.src = '/assets/icons/svg/icon-delete.svg';
-    cDelete.id = comment.id;
-    cDelete.alt = 'delete icon';
+   const cDelete = document.createElement('img');
+   cDelete.classList.add('live-comment__icon', 'live-comment__icon--delete');
+   cDelete.src = '/assets/icons/svg/icon-delete.svg';
+   cDelete.id = comment.id;
+   cDelete.alt = 'delete icon';
 
-    const left = document.createElement('div');
-    left.classList.add('live-comment__left');
-    left.append(avatar, likeBlock, cDelete);
+   const left = document.createElement('div');
+   left.classList.add('live-comment__left');
+   left.append(avatar, likeBlock, cDelete);
 
-    const right = document.createElement('div');
-    right.classList.add('live-comment__right');
-    right.append(name, date, details);
+   const right = document.createElement('div');
+   right.classList.add('live-comment__right');
+   right.append(name, date, details);
 
-    const liveComment = document.createElement('div');
-    liveComment.classList.add('live-comment');
-    liveComment.append(left, right);
+   const liveComment = document.createElement('div');
+   liveComment.classList.add('live-comment');
+   liveComment.append(left, right);
 
-    const timeDate = new Date(comment.timestamp);
-    const relative = timeDifference(timeDate);
+   const timeDate = new Date(comment.timestamp);
+   const relative = timeDifference(timeDate);
 
-    name.innerText = comment.name;
-    date.innerText = relative;
-    details.innerText = comment.comment;
+   name.innerText = comment.name;
+   date.innerText = relative;
+   details.innerText = comment.comment;
 
-    document.querySelector('.conversation__output').prepend(liveComment);
+   document.querySelector('.conversation__output').prepend(liveComment);
 }
 //provides a relative time difference from a time provided through the attributes
 timeDifference = (previous) => {
-    const current = new Date();
-    const msPerMinute = 60 * 1000;
-    const msPerHour = msPerMinute * 60;
-    const msPerDay = msPerHour * 24;
-    const msPerMonth = msPerDay * 30;
-    const msPerYear = msPerDay * 365;
+   const current = new Date();
+   const msPerMinute = 60 * 1000;
+   const msPerHour = msPerMinute * 60;
+   const msPerDay = msPerHour * 24;
+   const msPerMonth = msPerDay * 30;
+   const msPerYear = msPerDay * 365;
 
-    const elapsed = current - previous;
+   const elapsed = current - previous;
 
-    if (elapsed < msPerHour) {
-        return Math.round(elapsed / msPerMinute) + 1 + ' minutes ago';
-    }
-    else if (elapsed < msPerDay) {
-        return Math.round(elapsed / msPerHour) + ' hours ago';
-    }
-    else if (elapsed < msPerMonth) {
-        return Math.round(elapsed / msPerDay) + ' days ago';
-    }
-    else if (elapsed < msPerYear) {
-        return Math.round(elapsed / msPerMonth) - 1 + ' months ago';
-    }
-    else {
-        return Math.round(elapsed / msPerYear) + ' years ago';
-    }
+   if (elapsed < msPerHour) {
+      return Math.round(elapsed / msPerMinute) + 1 + ' minutes ago';
+   }
+   else if (elapsed < msPerDay) {
+      return Math.round(elapsed / msPerHour) + ' hours ago';
+   }
+   else if (elapsed < msPerMonth) {
+      return Math.round(elapsed / msPerDay) + ' days ago';
+   }
+   else if (elapsed < msPerYear) {
+      return Math.round(elapsed / msPerMonth) - 1 + ' months ago';
+   }
+   else {
+      return Math.round(elapsed / msPerYear) + ' years ago';
+   }
 }
 //function to create an event listener for 'like' clicks. sends put to api and updates html element innerText to current count
 listenForLikes = (button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        const addLike = axios.put(`${apiCommentsPage}${e.target.id}/like${apiKey}`);
-        addLike
-            .then(result => e.path[1].querySelector('.live-comment__like-count').innerText = result.data.likes)
-            .catch(error => console.warn(error))
-    })
+   button.addEventListener('click', (e) => {
+      e.preventDefault();
+      const addLike = axios.put(`${apiCommentsPage}${e.target.id}/like${apiKey}`);
+      addLike
+         .then(result => e.path[1].querySelector('.live-comment__like-count').innerText = result.data.likes)
+         .catch(error => console.warn(error))
+   })
 }
 //function creating event listener for 'delete' clicks. send delete method to api and update html to reflect new comment section
 listenForTrash = (button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        const deleteComment = axios.delete(apiCommentsPage + e.target.id + apiKey);
-        deleteComment
-            .then(() => e.target.parentNode.parentNode.innerHTML = "")
-            .catch(error => console.warn(error))
-    })
+   button.addEventListener('click', (e) => {
+      e.preventDefault();
+      const deleteComment = axios.delete(apiCommentsPage + e.target.id + apiKey);
+      deleteComment
+         .then(() => e.target.parentNode.parentNode.innerHTML = "")
+         .catch(error => console.warn(error))
+   })
 }
 // API requesting COMMENTS, sorts by timestamp
 const apiCommentsObj = axios.get(apiCommentsPage + apiKey)
 apiCommentsObj.then((result) => {
-    const sorted = result.data.sort((a, b) => (b.timestamp - a.timestamp));
-    sorted.forEach(comment => displayComment(comment));
-    const likeEl = document.querySelectorAll('.live-comment__icon--like');
-    likeEl.forEach((el) => listenForLikes(el));
-    
-    const trashEl = document.querySelectorAll('.live-comment__icon--delete');
-    trashEl.forEach((el, i, node) => listenForTrash(el));
+   const sorted = result.data.sort((a, b) => (b.timestamp - a.timestamp));
+   sorted.forEach(comment => displayComment(comment));
+   const likeEl = document.querySelectorAll('.live-comment__icon--like');
+   likeEl.forEach((el) => listenForLikes(el));
+
+   const trashEl = document.querySelectorAll('.live-comment__icon--delete');
+   trashEl.forEach((el, i, node) => listenForTrash(el));
 })
-    .catch(error => {
-    comments.forEach(comment => displayComment(comment));
-    console.warn(error)
-    })
+   .catch(error => {
+      comments.forEach(comment => displayComment(comment));
+      console.warn(error)
+   })
 
 // Event Listener for Form Submission
 const newComment = document.getElementById('newComment');
 newComment.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const newName = e.target.userName.value;
-    const newNameEl = e.target.userName;
-    const newCommentValue = e.target.userComment.value;
-    const newCommentEl = e.target.userComment;
-    const timestamp = new Date();
-    if (newName != "" && newCommentValue != "") {
-        newNameEl.classList.remove('comment__form--error');
-        newCommentEl.classList.remove('comment__form--error');
+   e.preventDefault();
+   const newName = e.target.userName.value;
+   const newNameEl = e.target.userName;
+   const newCommentValue = e.target.userComment.value;
+   const newCommentEl = e.target.userComment;
+   const timestamp = new Date();
+   if (newName != "" && newCommentValue != "") {
+      newNameEl.classList.remove('comment__form--error');
+      newCommentEl.classList.remove('comment__form--error');
 
-        const newCommentObject = {
-            id: 01,
-            timestamp: timestamp,
-            name: newName,
-            comment: newCommentValue
-        };
-        const apiCommentsPost = axios.post(apiCommentsPage + apiKey, {
-            'name': newName,
-            'comment': newCommentValue
-        })
-        apiCommentsPost.then(result => {
-            insertComment(result.data);
+      const newCommentObject = {
+         id: 01,
+         timestamp: timestamp,
+         name: newName,
+         comment: newCommentValue
+      };
+      const apiCommentsPost = axios.post(apiCommentsPage + apiKey, {
+         'name': newName,
+         'comment': newCommentValue
+      })
+      apiCommentsPost.then(result => {
+         insertComment(result.data);
 
-            const likeEl = document.querySelectorAll('.live-comment__icon--like');
-            likeEl.forEach((el) => listenForLikes(el));
+         const likeEl = document.querySelectorAll('.live-comment__icon--like');
+         likeEl.forEach((el) => listenForLikes(el));
 
-            const trashEl = document.querySelectorAll('.live-comment__icon--delete');
-            trashEl.forEach((el, i, node) => listenForTrash(el));
-        }).catch(error => {
-            comments.unshift(newCommentObject);
-            insertComment(newCommentObject);
-            console.warn(error);
-        })
-        newComment.reset();
-    }
-    else if (newName != "" && newCommentValue == "") {
-        newNameEl.classList.remove('comment__form--error');
-        newCommentEl.classList.add('comment__form--error');
-    }
-    else if (newName == "" && newCommentValue != "") {
-        newNameEl.classList.add('comment__form--error');
-        newCommentEl.classList.remove('comment__form--error');
-    }
-    else {
-        newNameEl.classList.add('comment__form--error');
-        newCommentEl.classList.add('comment__form--error');
-    }
+         const trashEl = document.querySelectorAll('.live-comment__icon--delete');
+         trashEl.forEach((el, i, node) => listenForTrash(el));
+      }).catch(error => {
+         comments.unshift(newCommentObject);
+         insertComment(newCommentObject);
+         console.warn(error);
+      })
+      newComment.reset();
+   }
+   else if (newName != "" && newCommentValue == "") {
+      newNameEl.classList.remove('comment__form--error');
+      newCommentEl.classList.add('comment__form--error');
+   }
+   else if (newName == "" && newCommentValue != "") {
+      newNameEl.classList.add('comment__form--error');
+      newCommentEl.classList.remove('comment__form--error');
+   }
+   else {
+      newNameEl.classList.add('comment__form--error');
+      newCommentEl.classList.add('comment__form--error');
+   }
 })
 
 
